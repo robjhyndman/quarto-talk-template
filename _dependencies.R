9583ef006be60997a110c2f419898e462d193f92
Rob Hyndman <robjhyndman@gmail.com>
2023-06-21 11:55:39 +1000

    Initial commit

---
+++ _dependencies.R
@@ -0,0 +1,8 @@
+# Some library commands to ensure packages end up in renv
+library(Cairo)
+library(codetools)
+library(httpuv)
+library(languageserver)
+library(markdown)
+library(ragg)
+library(styler)
